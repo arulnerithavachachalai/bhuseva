60c1e1d2546faaa01d1b1c2274675c68bf3ec7d7
arulnerithavachachalai <arulnerithavachachalai@gmail.com>
2025-12-12 02:08:03 +0530

    Remove basePath and assetPrefix from config
    Removed basePath and assetPrefix from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/bhuseva',
-  assetPrefix: '/bhuseva/',
   typescript: {
     ignoreBuildErrors: true,
   },
