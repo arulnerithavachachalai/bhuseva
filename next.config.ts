dd4c9cac70ec049d553c128c6bec882894ea01f6
D G YADHESH <arulnerithavachachalai@gmail.com>
2025-12-12 00:12:41 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: '/bhuseva',
+  assetPrefix: '/bhuseva/',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
